b12956eacbd759acbdfb77b3e4997ec217fc633d
Romeo RKPK <mindaye8504@gmail.com>
2022-02-22 12:04:32 +0900

    add event

--- src/main/java/org/lumenk/planet/flycontrol/planetuniverseflycontroller/listeners/PlayerJoinListener.java
+++ src/main/java/org/lumenk/planet/flycontrol/planetuniverseflycontroller/listeners/PlayerJoinListener.java
@@ -3,11 +3,13 @@ package org.lumenk.planet.flycontrol.planetuniverseflycontroller.listeners;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.lumenk.planet.flycontrol.planetuniverseflycontroller.contents.GeneralConfigProtoType;
 
 public class PlayerJoinListener implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event){
-
+        if(!GeneralConfigProtoType.getGenConfig().effective())return;
+        event.getPlayer().setAllowFlight(false);
     }
 }
